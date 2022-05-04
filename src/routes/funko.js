02802762ed13d3b3919b762e4bd720d6b6f518e1
Trompitas <90296185+Trompitas@users.noreply.github.com>
2022-05-04 07:30:47 -0500

    Update funko.js

--- src/routes/funko.js
+++ src/routes/funko.js
@@ -3,16 +3,6 @@ const funkoschema = require("../models/funkoM");
 
 const router = express.Router();
 
-// create funko
-router.post("/funkos", (req, res) => {
-  const funko = funkoschema(req.body);
-
-  funko
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-});
-
 // get all funkos
 router.get("/funkos", (req, res) => {
   funkoschema
@@ -35,16 +25,6 @@ router.delete("/funkos/:id", (req, res) => {
   const { id } = req.params;
   funkoschema
     .remove({ _id: id })
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-});
-
-// update a user
-router.put("/funkos/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, price, material, stock, coleccion, funkoImage, des } = req.body;
-  funkoschema
-    .updateOne({ _id: id }, { $set: { name, price, material, stock, coleccion, funkoImage, des } })
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
